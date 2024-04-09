02f5bc394cdc10570cb9b386b113845bae91e6ac
Ville Juutila <ville.juutila@gmail.com>
2024-04-09 13:48:45 +0300

    Refined connection handling logic

---
+++ src/utils/mod.rs
@@ -0,0 +1,28 @@
+#![allow(dead_code)]
+pub mod avl_record_builder;
+pub mod avl_frame_builder;
+pub mod avl_packet;
+
+#[cfg(test)]
+pub mod imei;
+
+/// Converts a hex string to a byte vector
+///
+/// # Arguments
+/// * `input` - The hex string to convert
+///
+/// # Returns
+/// * `Vec<u8>` - The byte vector
+pub fn str_to_bytes(input: &str) -> Vec<u8> {
+    let mut bytes: Vec<u8> = vec![];
+    for (i, char) in input.chars().enumerate() {
+        let val = if i % 2 != 0 {
+            format!("{}{}", input.chars().nth(i - 1).unwrap(), char)
+        } else {
+            continue;
+        };
+        bytes.push(u8::from_str_radix(&val, 16).unwrap())
+    }
+
+    return bytes;
+}
