eb9073a4b045bc3917fe11fc9894d453460f05e4
Ville Juutila <ville.juutila@gmail.com>
2024-11-20 10:35:41 +0200

    Re-generated specs

--- vehicle_management_service/src/apis/trucks_api.rs
+++ vehicle_management_service/src/apis/trucks_api.rs
@@ -70,6 +70,13 @@ pub struct DeleteTruckDriverCardParams {
     pub x_removed_at: String
 }
 
+/// struct for passing parameters to the method [`list_truck_driver_cards`]
+#[derive(Clone, Debug)]
+pub struct ListTruckDriverCardsParams {
+    /// truck ID
+    pub truck_id: String
+}
+
 
 /// struct for typed errors of method [`create_drive_state`]
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -115,6 +122,14 @@ pub enum CreateTruckSpeedError {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(untagged)]
 pub enum DeleteTruckDriverCardError {
+    DefaultResponse(models::Error),
+    UnknownValue(serde_json::Value),
+}
+
+/// struct for typed errors of method [`list_truck_driver_cards`]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum ListTruckDriverCardsError {
     DefaultResponse(models::Error),
     UnknownValue(serde_json::Value),
 }
@@ -368,6 +383,46 @@ pub async fn delete_truck_driver_card(configuration: &configuration::Configurati
         Ok(())
     } else {
         let local_var_entity: Option<DeleteTruckDriverCardError> = serde_json::from_str(&local_var_content).ok();
+        let local_var_error = ResponseContent { status: local_var_status, content: local_var_content, entity: local_var_entity };
+        Err(Error::ResponseError(local_var_error))
+    }
+}
+
+/// Lists truck driver cards. Used to check if a truck has a driver card inserted.
+pub async fn list_truck_driver_cards(configuration: &configuration::Configuration, params: ListTruckDriverCardsParams) -> Result<Vec<models::TruckDriverCard>, Error<ListTruckDriverCardsError>> {
+    let local_var_configuration = configuration;
+
+    // unbox the parameters
+    let truck_id = params.truck_id;
+
+
+    let local_var_client = &local_var_configuration.client;
+
+    let local_var_uri_str = format!("{}/vehicle-management/v1/trucks/{truckId}/driverCards", local_var_configuration.base_path, truckId=crate::apis::urlencode(truck_id));
+    let mut local_var_req_builder = local_var_client.request(reqwest::Method::GET, local_var_uri_str.as_str());
+
+    if let Some(ref local_var_user_agent) = local_var_configuration.user_agent {
+        local_var_req_builder = local_var_req_builder.header(reqwest::header::USER_AGENT, local_var_user_agent.clone());
+    }
+    if let Some(ref local_var_apikey) = local_var_configuration.api_key {
+        let local_var_key = local_var_apikey.key.clone();
+        let local_var_value = match local_var_apikey.prefix {
+            Some(ref local_var_prefix) => format!("{} {}", local_var_prefix, local_var_key),
+            None => local_var_key,
+        };
+        local_var_req_builder = local_var_req_builder.header("X-API-Key", local_var_value);
+    };
+
+    let local_var_req = local_var_req_builder.build()?;
+    let local_var_resp = local_var_client.execute(local_var_req).await?;
+
+    let local_var_status = local_var_resp.status();
+    let local_var_content = local_var_resp.text().await?;
+
+    if !local_var_status.is_client_error() && !local_var_status.is_server_error() {
+        serde_json::from_str(&local_var_content).map_err(Error::from)
+    } else {
+        let local_var_entity: Option<ListTruckDriverCardsError> = serde_json::from_str(&local_var_content).ok();
         let local_var_error = ResponseContent { status: local_var_status, content: local_var_content, entity: local_var_entity };
         Err(Error::ResponseError(local_var_error))
     }
